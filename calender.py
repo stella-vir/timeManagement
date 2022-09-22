14ea53025adc45bdad6d004a1e36cbe6edfcc1ed
stella-vir <stellavir11@gmail.com>
2022-09-22 22:16:50 +0300

    pytest add get self.schedules dict

--- calender.py
+++ calender.py
@@ -1,22 +1,32 @@
 # git create repo pages
 # curl -u 'stella-vir' https://api.github.com/user/repos -d '{"name":"timeManagement"}'
+# password: Personal access tokens
+# repo url: https://github.com/stella-vir/timeManagement
+
+weeks = [0, 1],
+week_days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+start_time = ['4:45', '9:00', '12:30'],
+end_time = ['13:15', '16:30', '20:00']
 
 class Calender:
+
     def __init__(self):
-        pass
+        self.schedules = []
+        # self.schedules = {
+        #     weeks: weeks,
+        #     week_days: week_days,
+        #     start_time: start_time,
+        #     end_time: end_time
+        # }
+
     def add(self, schedule):
-        dict = {
-            'weeks': [0, 1],
-            'week_days': ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-            'start_time': ['4:45', '9:00', '12:30'],
-            'end_time': ['13:15', '16:30', '20:00']
-            }
+        self.schedules.append(schedule)
 
     def size(self):
-        return 0
+        return len(self.schedules)
 
     def get_schedules(self):
-        pass
+        return self.schedules
 
     def get_gross_income(self):
         pass
--- test_calender.py
+++ test_calender.py
@@ -3,5 +3,14 @@ from calender import Calender
 
 def test_can_add_schedule_to_calender():
     s = Calender()
-    s.add('week_days': 'Tue')
+    # s.add({'week_days': 'Tue'})
+    s.add('Tue')
+
     assert s.size() == 1
+
+def test_when_schedule_added_then_calender_contains_schedule():
+    s = Calender()
+    # s.add({'week_days': 'Tue'})
+    s.add('Tue')
+
+    assert 'Tue' in s.get_schedules()
